c93db959f4f0a98ea62f7408233c78e8c4460865
Ерохов Евгений <prevgenij@gmail.com>
2018-10-19 09:29:02 +0200

    на первую проверку

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
-var navMain = navMain.querySelector('.main-nav');
-var navButtonOpen = navMain.querySelector('.page-header__button');
+var navMain = document.querySelector('.main-nav');
+var navButtonOpen = document.querySelector('.page-header__button');
 var navButtonClose = navMain.querySelector('.main-nav__toggle');
 
   navMain.classList.remove('main-nav--nojs');
