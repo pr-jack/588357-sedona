35fc788563047f6794bf7aa1442729b6e7d9ac91
Ерохов Евгений <prevgenij@gmail.com>
2018-10-24 01:21:34 +0200

    на первую проверку

--- source/js/script.js
+++ source/js/script.js
@@ -1,11 +1,8 @@
 'use strict';
 
-var navMain = document.querySelector('.main-nav');
 var navButtonOpen = document.querySelector('.page-header__button');
 var navButtonClose = navMain.querySelector('.main-nav__toggle');
 var navMain = document.querySelector('.main-nav');
-
-  navMain.classList.remove('main-nav--nojs');
 
   navButtonOpen.addEventListener('click', function() {
 
