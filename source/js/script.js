ede6df94246abd5e324f02f29b63dd92a29f890d
Ерохов Евгений <prevgenij@gmail.com>
2018-10-26 20:34:57 +0200

    на вторую проверку

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,8 @@
 'use strict';
 
+var navMain = document.querySelector('.main-nav');
 var navButtonOpen = document.querySelector('.page-header__button');
 var navButtonClose = navMain.querySelector('.main-nav__toggle');
-var navMain = document.querySelector('.main-nav');
 
   navButtonOpen.addEventListener('click', function() {
 
