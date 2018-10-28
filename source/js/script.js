49a3943cb1281182f657feaf777eeb511c03d6be
Ерохов Евгений <prevgenij@gmail.com>
2018-10-28 15:13:18 +0200

    на проверку

--- source/js/script.js
+++ source/js/script.js
@@ -3,11 +3,13 @@
 var navMain = document.querySelector('.main-nav');
 var navButtonOpen = document.querySelector('.page-header__button');
 var navButtonClose = navMain.querySelector('.main-nav__toggle');
+var headerBurger = document.querySelector('.page-header__burger');
 
   navButtonOpen.addEventListener('click', function() {
 
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
+    headerBurger.classList.add('page-header__burger--hidden');
   });
 
   navButtonClose.addEventListener('click', function() {
